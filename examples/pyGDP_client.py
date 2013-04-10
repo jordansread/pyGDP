33e7eaddf5973a63f55ceaa9c871f08e1a46940d
David Blodgett <dblodgett@usgs.gov>
2013-04-09 21:38:00 -0500

    need to do more work, commiting changes to date

--- examples/pyGDP_client.py
+++ examples/pyGDP_client.py
@@ -12,28 +12,60 @@ def getInput(listInput):
         usrinput = str(raw_input())
     return usrinput
 
+def getInput_2(listInput):
+
+    for i in listInput:
+        print i
+
+    print '\n' + 'Choose from the list above or press enter to chose all:'
+    usrinput = str(raw_input())
+    while usrinput not in listInput:
+        if usrinput =='':
+            break
+        print 'Not a valid input, please try again.'
+        usrinput = str(raw_input())
+    print usrinput
+    return usrinput
+
+def getInput_3(listInput):
+
+    for i in listInput:
+        print i
+
+    print '\n' + 'Choose an OPeNDAP url from the list above:'
+    usrinput = str(raw_input())
+    while 'dods' not in userinput:
+        print "This doesn't appear to be a valid dods url. Please enter an OPeNDAP url."
+        usrinput = str(raw_input())
+    return usrinput
+
+def getInput_4():
+
+    print 'Enter a search term or press enter to return all datasets in catalog.'
+    usrinput = str(raw_input())
+    return usrinput
+
 def main():
     gdp = pyGDP.pyGDPwebProcessing()
     sfiles = gdp.getShapefiles()
     for s in sfiles:
         print s
     
-    shapefile = 'sample:CONUS_States'
+    shapefile = getInput(sfiles)
     
     print
     print 'Get Attributes:'
     attributes = gdp.getAttributes(shapefile)
-    for a in attributes:
-        print a
-    
+    attribute = getInput(attributes)
+	
     print
     print 'Get values:'
-    values = gdp.getValues(shapefile, 'STATE')
-    for v in values:
-        print v
-        
+    values = gdp.getValues(shapefile, attribute)    
+    value = getInput_2(values)
+
     print
-    print 'Getting available Dataset URIS'
+    print 'Enter a search term or press enter to return all datasets in catalog.'
+    searchString = getInput_4()
     datasetURIs = gdp.getDataSetURI()
     dataSetURI = getInput(datasetURIs)
     
