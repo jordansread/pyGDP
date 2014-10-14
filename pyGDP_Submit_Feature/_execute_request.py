8542e9675bbf7c23a2d141bf81f4f182318b8353
David Blodgett <dblodgett@usgs.gov>
2014-10-14 16:17:25 -0500

    bringing back some error handling that was commented to supress prints.

--- pyGDP_Submit_Feature/_execute_request.py
+++ pyGDP_Submit_Feature/_execute_request.py
@@ -4,6 +4,7 @@ from pyGDP_Namespaces.pyGDP_Namespaces import URL_timeout, WPS_attempts
 from time import strftime, sleep
 import sys
 from os import devnull 
+from owslib.util import log
 
 def dodsReplace(dataSetURI, verbose=False):
     if "/dodsC" in dataSetURI:
@@ -23,16 +24,15 @@ def _executeRequest(processid, inputs, output, verbose=True, outputFilePath=None
     err_count=1
     
     while execution.isComplete()==False:
-        # try:
-        monitorExecution(execution, sleepSecs, download=False) # monitors for success
-        err_count=1
-        # except Exception:
-        #     print 'An error occurred while checking status, checking again.'
-        #     print 'Sleeping %d seconds...' % sleepSecs
-        #     err_count+=1
-        #     if err_count > WPS_attempts:
-        #         raise Exception('The status document failed to return, status checking has aborted. There has been a network or server issue preventing the status document from being retrieved, the request may still be running. For more information, check the status url %s' % execution.statusLocation)
-        #     sleep(sleepSecs)
+        try:
+            monitorExecution(execution, sleepSecs, download=False) # monitors for success
+            err_count=1
+        except Exception:
+            log.warning('An error occurred while checking status, checking again. Sleeping %d seconds...' % sleepSecs)
+            err_count+=1
+            if err_count > WPS_attempts:
+                raise Exception('The status document failed to return, status checking has aborted. There has been a network or server issue preventing the status document from being retrieved, the request may still be running. For more information, check the status url %s' % execution.statusLocation)
+            sleep(sleepSecs)
     
     if outputFilePath==None:
         outputFilePath='gdp_'+processid.replace('.','-')+'_'+strftime("%Y-%m-%dT%H-%M-%S-%Z")
@@ -40,15 +40,15 @@ def _executeRequest(processid, inputs, output, verbose=True, outputFilePath=None
     done=False
     err_count=1
     while done==False:
-        # try: 
-        monitorExecution(execution, download=True,filepath=outputFilePath)
-        done=True
-        # except Exception:
-        #             print 'An error occurred while trying to download the result file, trying again.'
-        #             err_count+=1
-        #             sleep(sleepSecs)
-        #         if err_count > WPS_attempts:        
-        #             raise Exception("The process completed successfully, but an error occurred while downloading the result. You may be able to download the file using the link at the bottom of the status document: %s" % execution.statusLocation)
+        try: 
+            monitorExecution(execution, download=True,filepath=outputFilePath)
+            done=True
+        except Exception:
+            log.warning('An error occurred while trying to download the result file, trying again.')
+            err_count+=1
+            sleep(sleepSecs)
+            if err_count > WPS_attempts:        
+                raise Exception("The process completed successfully, but an error occurred while downloading the result. You may be able to download the file using the link at the bottom of the status document: %s" % execution.statusLocation)
     
     _check_for_execution_errors(execution)
         
