0781cdfc6d4c679f6c23913ea62aabcab77b104b
David Blodgett <dblodgett@usgs.gov>
2015-04-04 07:36:01 -0500

    upping version

--- pyGDP.py
+++ pyGDP.py
@@ -17,7 +17,7 @@ import cgi
 import sys
 import logging
 
-__version__ = '1.3.1'
+__version__ = '1.3.2'
 
 #This series of import functions brings in the namespaces, url, and pyGDP utility
 #variables from the pyGDP_Namespaces file, as well as owslib's own namespaces
